90299caeb31b42bd879e75554c03a2ec726c4478
eneperezv <eneperezv@gmail.com>
2024-11-12 18:15:55 -0500

    Se incluye documentacion inicial de clases
     Changes to be committed:
    	modified:   todopro.api/src/main/java/com/enp/todopro/api/dto/CategoryDto.java
    	modified:   todopro.api/src/main/java/com/enp/todopro/api/dto/TodoDto.java
    	modified:   todopro.api/src/main/java/com/enp/todopro/api/model/Category.java
    	modified:   todopro.api/src/main/java/com/enp/todopro/api/model/Todo.java

--- todopro.api/src/main/java/com/enp/todopro/api/dto/CategoryDto.java
+++ todopro.api/src/main/java/com/enp/todopro/api/dto/CategoryDto.java
@@ -1,5 +1,20 @@
 package com.enp.todopro.api.dto;
 
+/*
+ * @(#)CategoryDto.java 1.0 12/11/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * @author eliezer.navarro
+ * @version 1.0
+ * @since 1.0
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
