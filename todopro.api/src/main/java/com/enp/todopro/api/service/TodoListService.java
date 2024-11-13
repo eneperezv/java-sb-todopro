d2a98a1258c9d341764996728f14459bd06c068c
eneperezv <eneperezv@gmail.com>
2024-11-12 19:05:01 -0500

    Implementacion de clases Repository y Service para gestion de TodoList
     Changes to be committed:
    	modified:   todopro.api/src/main/java/com/enp/todopro/api/repository/TodoListRepository.java
    	modified:   todopro.api/src/main/java/com/enp/todopro/api/service/TodoListService.java

--- todopro.api/src/main/java/com/enp/todopro/api/service/TodoListService.java
+++ todopro.api/src/main/java/com/enp/todopro/api/service/TodoListService.java
@@ -1,12 +1,27 @@
 package com.enp.todopro.api.service;
 
+/*
+ * @(#)TodoListService.java 1.0 12/11/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+/**
+ * @author eliezer.navarro
+ * @version 1.0
+ * @since 1.0
+ */
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.enp.todopro.api.dto.CategoryDto;
-import com.enp.todopro.api.model.Category;
+import com.enp.todopro.api.dto.TodoListDto;
+import com.enp.todopro.api.model.TodoList;
 import com.enp.todopro.api.repository.TodoListRepository;
 
 @Service
@@ -15,31 +30,34 @@ public class TodoListService {
 	@Autowired
 	private TodoListRepository todoListRepository;
 	
-	public Category buildEntity(CategoryDto categoryDto) {
-		Category category = new Category();
-		category.setId(categoryDto.getId());
-		category.setName(categoryDto.getName());
-		category.setTodos(categoryDto.getTodos());
+	public TodoList buildEntity(TodoListDto todoListDto) {
+		TodoList todoList = new TodoList();
+		todoList.setId(todoListDto.getId());
+		todoList.setTitle(todoListDto.getTitle());
+		todoList.setUser(todoListDto.getUser());
+		todoList.setTodos(todoListDto.getTodos());
 		
-		return category;
+		return todoList;
 	}
 	
-	public CategoryDto buildDto(Optional<Category> optional) {
-		CategoryDto categoryDto = new CategoryDto();
-		categoryDto.setId(optional.get().getId());
-		categoryDto.setName(optional.get().getName());
-		categoryDto.setTodos(optional.get().getTodos());
+	public TodoListDto buildDto(Optional<TodoList> optional) {
+		TodoListDto todoListDto = new TodoListDto();
+		todoListDto.setId(optional.get().getId());
+		todoListDto.setTitle(optional.get().getTitle());
+		todoListDto.setUser(optional.get().getUser());
+		todoListDto.setTodos(optional.get().getTodos());
 		
-		return categoryDto;
+		return todoListDto;
 	}
 	
-	public CategoryDto buildDtoFromCategory(Category category) {
-		CategoryDto categoryDto = new CategoryDto();
-		categoryDto.setId(category.getId());
-		categoryDto.setName(category.getName());
-		categoryDto.setTodos(category.getTodos());
+	public TodoListDto buildDtoFromCategory(TodoList todoList) {
+		TodoListDto todoListDto = new TodoListDto();
+		todoListDto.setId(todoList.getId());
+		todoListDto.setTitle(todoList.getTitle());
+		todoListDto.setUser(todoList.getUser());
+		todoListDto.setTodos(todoList.getTodos());
 		
-		return categoryDto;
+		return todoListDto;
 	}
 
 }
